1e8f5cad64d4436e76f5ecabf3b066435b2ce879
DSMalamov <dsmalamov@gmail.com>
2023-01-13 00:28:36 +0200

    problems solved

--- StacksAndQueues-Exercise/06.SongsQueue/Program.cs
+++ StacksAndQueues-Exercise/06.SongsQueue/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            
+            Queue<string> songs = new(Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries));
+
+            while (songs.Any())
+            {
+                string[] commnad = Console.ReadLine()
+                    .Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+                string cmdArg = commnad[0];
+
+                if (cmdArg == "Play")
+                {
+                    songs.Dequeue();
+                }
+                else if (cmdArg == "Add")
+                {
+                    string song = string.Join(" ", commnad.Skip(1));
+
+                    if (!songs.Contains(song))
+                    {
+                        songs.Enqueue(song);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+
+                }
+                else if (cmdArg == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", songs));
+                }
+                
+            }
+
+            Console.WriteLine("No more songs!");
         }
     }
 }
