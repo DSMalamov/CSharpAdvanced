42c4d62258bc893b78bf9d4375274d446a534975
DSMalamov <dsmalamov@gmail.com>
2023-01-20 19:14:02 +0200

    Problems Solved

---
+++ DictionariesAdvanced-Lab/03.Largest3Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace _03.Largest3Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .OrderByDescending(x => x)
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ", numbers.Take(3)));
+
+            
+        }
+    }
+}
