f4fd7473f4bc0b332ac1dc76f9c0deeab8c7534a
DSMalamov <dsmalamov@gmail.com>
2023-01-21 11:04:32 +0200

    Problems Solved

---
+++ DictionariesAdvanced-Lab/05.CitiesContinentCountry/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Numerics;
+
+namespace _05.CitiesContinentCountry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, List<string>>> dictionary = 
+                new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (!dictionary.ContainsKey(continent))
+                {
+                    dictionary.Add(continent, new Dictionary<string, List<string>>());
+
+                }
+
+                if (!dictionary[continent].ContainsKey(country))
+                {
+                    dictionary[continent].Add(country, new List<string>());
+                }
+
+                dictionary[continent][country].Add(city);
+
+            }
+
+            //Europe:
+            //      Bulgaria->Sofia, Plovdiv
+            //      Poland->Warsaw, Poznan
+            //      Germany->Berlin
+
+            foreach (var continent in dictionary)
+            {
+                Console.WriteLine($"{continent.Key}:");
+
+                foreach (var country in continent.Value)
+                {
+                    Console.Write($"  {country.Key} -> ");
+                    Console.WriteLine(string.Join(", ", country.Value));
+                }
+            }
+        }
+    }
+}
