a3388b8b2ccc44c6c626291a8dac62ead835a3a5
DSMalamov <dsmalamov@gmail.com>
2023-01-23 22:52:46 +0200

    Problems Solved

---
+++ Skeleton-Lab/LineNumbers/LineNumbers.cs
@@ -0,0 +1,42 @@
+﻿namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+
+            using (var input = new StreamReader(inputFilePath))
+            {
+                string line = input.ReadLine();
+                int num = 1;
+
+                
+                    using (var output = new StreamWriter(outputFilePath))
+                    {
+                        while (line != null)
+                        {
+                            output.WriteLine($"{num}. {line}");
+                            num++;
+                            line = input.ReadLine();
+                        }
+                        
+                    }
+                
+                
+
+
+            }
+
+
+        }
+    }
+}
