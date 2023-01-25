56dc45435fc18bc2c9126bb2e43075c4f81be4e7
DSMalamov <dsmalamov@gmail.com>
2023-01-25 23:46:28 +0200

    Problems Solved

---
+++ FunctionalProgramming-Lab/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Predicate<string> chacker = w => w[0] == w.ToUpper()[0];
+
+string[] input = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Where(w => chacker(w))
+    .ToArray();
+
+foreach (var item in input)
+{
+    Console.WriteLine(item);
+}
---
+++ FunctionalProgramming-Lab/04.AddVAT/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+string input = Console.ReadLine();
+Func<string, double> parser = n => double.Parse(n);
+Func<double, double> multiplyer = n => n * 1.2; 
+
+double[] numbers = input
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(parser)
+    .Select(multiplyer)
+    .ToArray();
+
+foreach (var number in numbers)
+{
+    Console.WriteLine($"{number:f2}");
+}
