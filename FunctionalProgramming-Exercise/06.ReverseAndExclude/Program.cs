f3aa26b8724c761e227ca2a5e5aebd63bce543e0
DSMalamov <dsmalamov@gmail.com>
2023-01-26 22:28:47 +0200

    Problems Solved

---
+++ FunctionalProgramming-Exercise/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int[] input = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int number = int.Parse(Console.ReadLine());
+
+Predicate<int> Match = n => n % number != 0;
+
+Func<int[], Predicate<int>, List<int>> function = (ar, m) =>
+{
+    List<int> numbers = new();
+
+    for (int i = 0; i < ar.Length; i++)
+    {
+        if (m(ar[i]))
+        {
+            numbers.Add(ar[i]);
+        }
+    }
+    return numbers;
+};
+
+List<int> result = function(input, Match);
+    result.Reverse();
+
+Console.WriteLine(string.Join(" ", result));
