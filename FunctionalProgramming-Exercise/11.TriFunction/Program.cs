11ec370797d9da0328b80ce22690df66628cbc42
DSMalamov <dsmalamov@gmail.com>
2023-01-28 14:14:22 +0200

    Problems Solved

---
+++ FunctionalProgramming-Exercise/11.TriFunction/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+Func<string, int, bool> function = (n, h) => n.Sum(ch => ch) >= h;
+
+
+Func<string[], Func<string, int, bool>, int, string> Logic = (names, value, sum) => names
+.FirstOrDefault(name => value(name, sum));
+
+int n = int.Parse(Console.ReadLine());
+
+string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Console.WriteLine(Logic(names, function, n));
