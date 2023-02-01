32b9267432f3d9abb65ca097ded3bda0af827605
DSMalamov <dsmalamov@gmail.com>
2023-02-01 23:04:39 +0200

    Problems Solved

---
+++ DefiningClasses-Exercise/06.SpeedRacing/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.SpeedRacing
+{
+    public class Car
+    {
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKm)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKm = fuelConsumptionPerKm;
+            TravelledDist = 0;
+        }
+
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumptionPerKm { get; set; }
+        public double TravelledDist { get; set; }
+
+        public Car Travel(string name, int distance)
+        {
+            if ( )
+            {
+
+            }
+
+        }
+    }
+}
---
+++ DefiningClasses-Exercise/06.SpeedRacing/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _06.SpeedRacing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] currCar = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Car car = new(currCar[0], double.Parse(currCar[1]), double.Parse(currCar[2]));
+
+                cars.Add(car);
+            }
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] cmdArg = command
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                
+            }
+        }
+    }
+}
