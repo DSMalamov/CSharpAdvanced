700046333ad8d0489f503c399276e23df4c223ec
DSMalamov <dsmalamov@gmail.com>
2023-02-07 22:32:13 +0200

    ProblemsSolved

---
+++ BoxOfT/ArrayCreator/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string[] joros = ArrayCreator.Create(15, "joro");
+
+            Console.WriteLine(String.Join(" ", joros));
+        }
+    }
+}
---
+++ BoxOfT/BoxOfT/Box.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> list;
+        private int count;
+        public Box()
+        {
+            list = new List<T>();
+        }
+        public int Count { get { return this.count; } }
+
+        public void Add(T element)
+        {
+            count++;
+            list.Add(element);
+        }
+
+        public T Remove()
+        {
+            count--;
+            T removedEl = list[list.Count - 1];
+            list.Remove(removedEl);
+
+            return removedEl;
+        }
+
+        
+
+    }
+}
