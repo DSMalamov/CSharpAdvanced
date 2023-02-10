6a7a89b468fe83e1fe6c305924ed2d0ff0864c6e
DSMalamov <dsmalamov@gmail.com>
2023-02-10 21:37:55 +0200

    ProblemsSolved

---
+++ ExerciseGenerics/GenericGetCountMethodDouble/Box.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericGetCountMethodDouble
+{
+    internal class Box<T> where T : IComparable<T> 
+    {
+        private List<T> list;
+
+        public Box()
+        {
+            list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            list.Add(item);
+        }
+
+        public void Swap(int index1, int index2)
+        {
+            (list[index1], list[index2]) = (list[index2], list[index1]);
+
+        }
+
+        public int Count(T itemToCompare)
+        {
+            int count = 0;
+
+            foreach (T item in list)
+            {
+                if (item.CompareTo(itemToCompare) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (T item in list)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
